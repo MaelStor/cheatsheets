da2bc020233586f9e44258bac82adaa1fa9cfb25
lenny <lenny@simstome>
2019-04-22 01:29:49 +0200

    build.sh: Make build.sh run with pandoc >= 2.0.0
    Better usage of bash constructs
    
    Reformat to use 2 spaces indent
    
    Use new pandoc option --pdf-engine instead of
      --latex-engine
    
    Minor changes in variable naming

--- build.sh
+++ build.sh
@@ -2,65 +2,73 @@
 
 ###############
 # Ensure in correct location
-MY_PATH="`dirname \"$0\"`"              # relative
-MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
-cd $MY_PATH
+MY_PATH=$(dirname "$0")             # relative
+MY_PATH=$(readlink -e "$MY_PATH") # absolutized and normalized
+cd "$MY_PATH" || exit 1
 ###############
 
+TEMPLATE="$MY_PATH/templates/cheatsheet.latex"
+
 FILTER="$1"
+FILTER_ARG="$2"
 
 LINKS_OUT="CHEATSHEETS.md"
 
 function pandoc_build() {
-    local $*
-    TEMPLATE="$MY_PATH/templates/cheatsheet.latex"
-    IN_PATH="$IN/$name.md"
+  local name
+  name="$1"
 
-    #################################
-    # Provides the option of selecting only certain cheatsheets
-    # If we specify a path instead of a search param, lets check for that too (used by entr)
-    if [ -n "$FILTER" ] && [[ ! $name =~ "$FILTER" ]] && [[ ! "$FILTER" -ef "$IN_PATH" ]]; then
-        return
-    fi
+  in_path="$IN/$name.md"
+  out_name="${OUT}/${name}"
 
-    echo "$name  ->  $OUT/$name.pdf"
-    pandoc\
-        -f markdown\
-        "$IN_PATH"\
-        --variable=cohort:$COHORT\
-        -t latex\
-        --template "$TEMPLATE"\
-        --latex-engine pdflatex\
-        --toc-depth 1\
-        -o "$OUT/$name.pdf"
+  #################################
+  # Provides the option of selecting only certain cheatsheets
+  # If we specify a path instead of a search param, lets check for that too (used by entr)
+  if [[ "$FILTER" ]] && [[ ! $name == "$FILTER" ]] && [[ ! "$FILTER" -ef "$in_path" ]]; then
+    return 1
+  fi
 
-    if [ -x "$(command -v convert)" ]; then
-        convert "$OUT/$name.pdf" -background "#FFFFFF" -resize 350x -flatten "$OUT/$name.thumb.jpg" 
-        convert -density 300 "$OUT/$name.pdf" -resize 50% -flatten "$OUT/$name.jpg" 
-    fi
+  echo "${name}  ->  ${out_name}.pdf"
+  pandoc \
+    -f markdown \
+    "$in_path" \
+    --variable=cohort:"$COHORT" \
+    -t latex \
+    --template "$TEMPLATE" \
+    --pdf-engine='pdflatex' \
+    -o "${out_name}.pdf" \
+    --toc-depth 1
 
+  if [[ -x "$(command -v convert)" ]]; then
+    convert "${out_name}.pdf" -background "#FFFFFF" -resize 350x -flatten "${out_name}.thumb.jpg"
+    convert -density 300 "${out_name}.pdf" -resize 50% -flatten "${out_name}.jpg"
+  fi
 
-    ## Handle creating the LINKS.md markdown file (only when building all)
-    if ! [ -n "$FILTER" ]; then
-        echo "
+  ## Handle creating the LINKS.md markdown file (only when building all)
+  if [[ -z "$FILTER" ]]; then
+    cat << EOF >> "$LINKS_OUT"
 ### $name
+[![$name PDF thumbnail](./${out_name}.thumb.jpg)](https://github.com/kickstartcoding/cheatsheets/raw/master/${out_name}.pdf)
 
-[![$name PDF thumbnail](./$OUT/$name.thumb.jpg)](https://github.com/kickstartcoding/cheatsheets/raw/master/$OUT/$name.pdf)
-" >> "$LINKS_OUT"
-    fi
+EOF
+  fi
 
+  return 0
 }
 
-if [ -n "$FILTER" ] && [ $FILTER = "--watch" ]; then
+if [[ "$FILTER" ]] && [[ $FILTER == "--watch" ]]; then
+  if [[ "$FILTER_ARG" ]]; then
+    dir=$(dirname "$FILTER_ARG")
+    file=$(basename "$FILTER_ARG")
     echo "Entering watch mode..."
-    ls $2.md | entr -p ./build.sh /_
-    exit 0
+    find "$dir" -name "${file}.md" | entr -p ./build.sh /_
+    exit
+  fi
 fi
 
-
 ## Handle creating the LINKS.md markdown file (only when building all)
-if ! [ -n "$FILTER" ]; then
-    echo "
+if [[ -z "$FILTER" ]]; then
+  echo   "
 # Cheatsheets
 
 Click on one of the following thumbnails to download one of our cheatsheets.
@@ -68,39 +76,37 @@ Click on one of the following thumbnails to download one of our cheatsheets.
 " > "$LINKS_OUT"
 fi
 
-
 COHORT="2019-Winter"
 IN="kickstart-backend"
 OUT="build/kickstart-backend"
-pandoc_build name="1-html-css"
-pandoc_build name="2-bash-git"
-pandoc_build name="3-python"
-pandoc_build name="4-modules-oop"
-pandoc_build name="5-http"
-pandoc_build name="6-django"
-pandoc_build name="7-orm"
-pandoc_build name="8-postgres"
-
+pandoc_build "1-html-css"
+pandoc_build "2-bash-git"
+pandoc_build "3-python"
+pandoc_build "4-modules-oop"
+pandoc_build "5-http"
+pandoc_build "6-django"
+pandoc_build "7-orm"
+pandoc_build "8-postgres"
 
 IN="kickstart-frontend"
 OUT="build/kickstart-frontend"
-pandoc_build name="1-css"
-pandoc_build name="2-javascript"
-pandoc_build name="3-react"
-pandoc_build name="4-components"
-pandoc_build name="5-mern"
-pandoc_build name="6-industry"
-pandoc_build name="7-algorithms"
-
+pandoc_build "1-css"
+pandoc_build "2-javascript"
+pandoc_build "3-react"
+pandoc_build "4-components"
+pandoc_build "5-mern"
+pandoc_build "6-industry"
+pandoc_build "7-algorithms"
 
 IN="topical"
 OUT="build/topical"
 COHORT="Cheatsheet"
-pandoc_build name="javascript-for-pythonistas"
-pandoc_build name="python-for-js-developers"
-pandoc_build name="python"
-pandoc_build name="modern-html-css"
-pandoc_build name="django"
-pandoc_build name="cli-bash"
-pandoc_build name="react-redux"
+pandoc_build "javascript-for-pythonistas"
+pandoc_build "python-for-js-developers"
+pandoc_build "python"
+pandoc_build "modern-html-css"
+pandoc_build "django"
+pandoc_build "cli-bash"
+pandoc_build "react-redux"
 
+exit 0
